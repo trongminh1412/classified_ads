6a90ae4520b17b3d1e77a4032d5ab9de1abd3418
Minh <43989815+trongminh1412@users.noreply.github.com>
2022-01-14 00:02:01 +0700

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Navigation from 'components/Navigation/Navigation_index';
 import Popular from 'components/carousel/popular';
 import Footer from 'components/Footers/footer';
 import Latest from 'components/Home_product/Latest';
+import Category from 'components/Home_product/Category';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { Container, Row, Col, Input, Button, Card, CardBody } from 'reactstrap';
 export default function Home() {
@@ -131,188 +132,7 @@ export default function Home() {
                         </h6>
                       </div>
                       <div className="s-category__content__body">
-                        <Row>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/automobile.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Automobiles</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/mobile.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Phones & Tablets</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/electronics.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Electronics</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/furniture.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Furniture & Appliances</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/real.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Real estate</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/animals.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Animals & Pets</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/fashion.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Fashion</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/beauty.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Beauty & Well being</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/job.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Jobs</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/services.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Services</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/real.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Learning</h6>
-                            </div>
-                          </Col>
-                          <Col xl="2" lg="3" md="4" sm="6" className="pt-4">
-                            <div className="product">
-                              <div className="product_spin">
-                                <Image
-                                  src="/home/category/event.svg"
-                                  alt="7Hit Logo"
-                                  width={60}
-                                  height={60}
-                                  layout="intrinsic"
-                                  priority="true"
-                                />
-                              </div>
-                              <h6>Local Events</h6>
-                            </div>
-                          </Col>
-                        </Row>
+                        <Category />
                       </div>
                     </div>
                   </Container>
@@ -331,38 +151,7 @@ export default function Home() {
                         </h6>
                       </div>
                       <div className="s-latest__content__body">
-                        <Row className="row-cols-xl-5 row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1">
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                          <Col className="py-2">
-                            <Latest />
-                          </Col>
-                        </Row>
+                        <Latest />
                       </div>
                     </Container>
                   </div>
