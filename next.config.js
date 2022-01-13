a3abbdc22fe439e0f178dbe595d527dede832b8c
Minh <43989815+trongminh1412@users.noreply.github.com>
2022-01-13 14:57:06 +0700

    update config env variables

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ module.exports = {
   publicRuntimeConfig: {
     apiUrl:
       process.env.NODE_ENV === 'development'
-        ? process.env.API_URL // development api
-        : process.env.API_URL, // production api
+        ? process.env.NEXT_PUBLIC_API_URL // development api
+        : process.env.NEXT_PUBLIC_API_URL, // production api
   },
   images: {
     domains: ['https:/img-7hit-com.s3.ap-northeast-2.amazonaws.com'],
