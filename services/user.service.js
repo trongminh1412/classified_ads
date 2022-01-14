3c6d19620e53ca4516e9fe65965e122fee1b85d2
Minh <43989815+trongminh1412@users.noreply.github.com>
2022-01-14 16:04:32 +0700

    update logout to homepage

--- services/user.service.js
+++ services/user.service.js
@@ -36,7 +36,7 @@ function logout() {
   // remove user from local storage, publish null to user subscribers and redirect to login page
   localStorage.removeItem('user');
   userSubject.next(null);
-  Router.push('/auth/login');
+  Router.push('/home');
 }
 
 function register(user) {
