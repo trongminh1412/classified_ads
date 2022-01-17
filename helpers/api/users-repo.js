2934559640381a35fda944a40866ca178a8da6d6
Minh <43989815+trongminh1412@users.noreply.github.com>
2022-01-17 13:30:54 +0700

    fix bug

--- helpers/api/users-repo.js
+++ helpers/api/users-repo.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // users in JSON file for simplicity, store in a db for production applications
-let users = require('/data/users.json');
+let users = require('data/users.json');
 
 export const usersRepo = {
   getAll: () => users,
@@ -46,5 +46,5 @@ function _delete(id) {
 // private helper functions
 
 function saveData() {
-  fs.writeFileSync('/data/users.json', JSON.stringify(users, null, 4));
+  fs.writeFileSync('data/users.json', JSON.stringify(users, null, 4));
 }
