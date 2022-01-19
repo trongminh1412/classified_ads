e9436ea296c7fc2a84c448ae7fba8d729956dff5
Minh Nguyen <minh.nguyen@headcapital.com>
2022-01-20 00:30:05 +0700

    update product

--- components/Product/Latest.js
+++ components/Product/Latest.js
@@ -1,11 +1,5 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { BsClock } from 'react-icons/bs';
-import { AiOutlineFolder } from 'react-icons/ai';
-import { GoLocation } from 'react-icons/go';
-import { CgChevronDoubleRight } from 'react-icons/cg';
-import { VscBookmark } from 'react-icons/vsc';
-import { Col, Card, CardBody } from 'reactstrap';
+import { Col } from 'reactstrap';
+import Product from './Product';
 
 export const data = [
   '/home/popular/1.svg',
@@ -20,73 +14,12 @@ export const data = [
   '/home/popular/2.svg',
 ];
 
-const imageLatest = ({ src, width, quality }) => {
-  return `${process.env.NEXT_PUBLIC_APP_URL}/${src}?w=${width}&q=${
-    quality || 75
-  }`;
-};
-
 function latest() {
   return (
     <>
-      {data.map((id, index) => (
+      {data.map((src, index) => (
         <Col className="py-2" key={index}>
-          <div className="card__item">
-            <Card>
-              <Link href="/productDetail" passHref>
-                <a>
-                  <Image
-                    loader={imageLatest}
-                    src={id}
-                    alt="collection kids"
-                    layout="responsive"
-                    width={154}
-                    height={154}
-                    priority="true"
-                  />
-                </a>
-              </Link>
-              <div className="card__item--sub-img">
-                <Image
-                  src="/home/popular/sub_img.svg"
-                  alt="collection kids"
-                  layout="intrinsic"
-                  width={12}
-                  height={12}
-                  priority="true"
-                />
-                <span className="ps-1">5</span>
-              </div>
-              <CardBody className="text-start px-2">
-                <h6 className="fs-14 fw-bold py-1text-reset">
-                  <Link href="/productDetail">
-                    <a className="text-reset">Iphone 12 white color</a>
-                  </Link>
-                </h6>
-                <div className="fs-10 py-1">
-                  <BsClock /> 13h hours ago
-                </div>
-                <div className="fs-10 py-1 d-flex">
-                  <AiOutlineFolder />
-                  <div className="ps-1">
-                    Phone & Tablets
-                    <span className="text-warning">
-                      <CgChevronDoubleRight /> Mobile phones
-                    </span>
-                  </div>
-                </div>
-                <div className="fs-10 py-1">
-                  <GoLocation /> Ellsworth
-                </div>
-                <div className="d-flex justify-content-between align-items-center py-1 text_price">
-                  <h6 className="mb-0">$49,192</h6>
-                  <div className="bookmark">
-                    <VscBookmark />
-                  </div>
-                </div>
-              </CardBody>
-            </Card>
-          </div>
+          <Product src={src} />
         </Col>
       ))}
     </>
